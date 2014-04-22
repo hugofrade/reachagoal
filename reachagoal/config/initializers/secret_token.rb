9667f7be14eaaee417b050ca29c035059ad0f0bd
Hugo Frade <hugoecfrade@gmail.com>
2014-04-22 19:42:09 +0100

    App Structure Created

---
+++ reachagoal/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reachagoal::Application.config.secret_key_base = '43d25f6a85fed741210e40c1f85db012e95e3d841b845e63a61d5455d2309e4bf6b4f8fc6ef560ae2e05b70d9ee462a2642903658dd690fad6fb2df42b542e10'
---
+++ reachagoal/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Reachagoal::Application.config.session_store :cookie_store, key: '_reachagoal_session'
